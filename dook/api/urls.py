f55649fbbadd74a5a80ffd7e024dd176656b388c
Jaroslaw Galinski <jaroslaw.galinski@gmail.com>
2020-05-08 10:15:51 +0200

    moved templates to another directory

--- dook/api/urls.py
+++ dook/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", lambda r: HttpResponse()),
-    path(r"^admin/", include(("dook.api.admin.urls", "admin"), namespace="admin")),
-    path(r"^api/", include(("dook.api.news.urls", "api"), namespace="api")),
-    path(r"^users/", include(("dook.api.users.urls", "users"), namespace="users")),
+    path("admin/", include(("dook.api.admin.urls", "admin"), namespace="admin")),
+    path("api/", include(("dook.api.news.urls", "api"), namespace="api")),
+    path("users/", include(("dook.api.users.urls", "users"), namespace="users")),
 ]
